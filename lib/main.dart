41369f3199fc3b9f7cefe13196c852258fde9350
Tobey <TobeyG2003@gmail.com>
2025-09-22 20:08:17 -0400

    adds input text box

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
 
 void main() {
   runApp(MaterialApp(
@@ -15,6 +16,14 @@ class _DigitalPetAppState extends State<DigitalPetApp> {
   String petName = "Your Pet";
   int happinessLevel = 50;
   int hungerLevel = 50;
+  Color happyColor = Colors.yellow;
+  String currentLevel = "Neutral";
+
+  void changeName(String name) {
+    setState(() {
+      petName = name;
+    });
+  }
 
   void _playWithPet() {
     setState(() {
@@ -32,9 +41,16 @@ class _DigitalPetAppState extends State<DigitalPetApp> {
 
   void _updateHappiness() {
     if (hungerLevel < 30) {
-      happinessLevel -= 20;
+      happinessLevel += 20; //swapped these values since lower hunger should increase happiness
     } else {
-      happinessLevel += 10;
+      happinessLevel -= 10;
+    }
+    if (happinessLevel > 70) {
+      happyColor = Colors.green;
+    } else if (happinessLevel >= 30) {
+      happyColor = Colors.yellow;
+    } else {
+      happyColor = Colors.red;
     }
   }
 
@@ -58,17 +74,43 @@ class _DigitalPetAppState extends State<DigitalPetApp> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
+            SizedBox(width: 300,
+            child: TextField(
+              decoration: InputDecoration(
+                labelText: 'Enter your pet\'s name',
+                border: OutlineInputBorder(),
+              ),
+              onSubmitted: (text) {
+                changeName(text);
+              },
+            ),
+            ),
+            SizedBox(height: 16.0),
             Text(
               'Name: $petName',
               style: TextStyle(fontSize: 20.0),
             ),
+            ColorFiltered(
+              colorFilter: ColorFilter.mode(happyColor, BlendMode.modulate),
+              child:
             Image.asset('asset/dog-png-30.png'
-                , height: 200, width: 200),
+                , height: 200, width: 200),),
             SizedBox(height: 16.0),
-            Text(
-              'Happiness Level: $happinessLevel',
-              style: TextStyle(fontSize: 20.0),
-            ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'Happiness Level: $happinessLevel ($currentLevel) ',
+                style: TextStyle(fontSize: 20.0),
+              ),
+              if (happinessLevel > 70) 
+                Icon(Icons.sentiment_very_satisfied, color: Colors.green, size: 30)
+              else if (happinessLevel >= 30)
+                Icon(Icons.sentiment_satisfied, color: Colors.yellow, size: 30)
+              else
+                Icon(Icons.sentiment_dissatisfied, color: Colors.red, size: 30),
+            ],
+          ),
             SizedBox(height: 16.0),
             Text(
               'Hunger Level: $hungerLevel',
